8d2f6ec5009041adc00db049dc3172f7f95389d8
D Spears <dspears@speartech.net>
2022-07-15 16:05:06 -0500

    Added Wall Jumping Mechanic
    Added wall jumping mechanic.
    In separate, GameDevHQ provided SampleScene.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,11 +12,15 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private Vector3 _playerVelocity;
     [SerializeField] private bool _isGrounded;
     [SerializeField] private float _playerSpeed;
+    [SerializeField] private float _jumpHeight;
     [SerializeField] private bool _startJump;
     [SerializeField] private int _currNumOfJumps;
-    [SerializeField] private float _jumpHeight;
+    [SerializeField] private bool _canWallJump;
+    [SerializeField] bool _isWallJumping;
     [SerializeField] private float _gravityValue;
     [SerializeField] private bool _movementDisabled;
+    private Vector3 _wallCollisionNormal;
+
 
     private void OnEnable() {
         _inputActions = new InputActions();
@@ -25,54 +29,51 @@ public class PlayerController : MonoBehaviour {
         _inputActions.Player.Jump.performed += JumpOnperformed;
         _currNumOfJumps = 0;
         _movementDisabled = false;
+        _canWallJump = false;
+        _isWallJumping = false;
     }
-
+    
     private void OnDisable() {
         _inputActions.Player.Movement.performed -= MovementOnperformed;
         _inputActions.Player.Jump.performed -= JumpOnperformed;
     }
 
-    private void MovementOnperformed(InputAction.CallbackContext context) {
-        
-    }
+    private void MovementOnperformed(InputAction.CallbackContext context) { }
 
     private void JumpOnperformed(InputAction.CallbackContext context) {
-        _startJump = true; 
-        _currNumOfJumps += 1; 
-        transform.SetParent(null);
+        if (_canWallJump) { _canWallJump = false; _isWallJumping = true; //wall jump
+        } else { _startJump = true; _currNumOfJumps += 1; transform.SetParent(null); } //start normal jump
     }
 
-    void Start() {
-        _controller = GetComponent<CharacterController>();
-        DoNullChecks();
-    }
+    void Start() { _controller = GetComponent<CharacterController>(); DoNullChecks(); }
     
-    void FixedUpdate() {
-        Vector2 moveDirection = _inputActions.Player.Movement.ReadValue<Vector2>();
-        float moveX = moveDirection.x;
-        
+    void FixedUpdate() { DeterminePlayerVelocity(); }
+
+    private void DeterminePlayerVelocity() {
+        Vector3 moveDirection = _inputActions.Player.Movement.ReadValue<Vector2>();
         _isGrounded = _controller.isGrounded;
-        
-        if (_isGrounded) { _currNumOfJumps = 0;}
-        if (_isGrounded && _playerVelocity.y < 0) { _playerVelocity.y = 0; }
 
-        if (!_movementDisabled) { MovePlayer(new Vector3(moveX, 0f, 0f)); }
-    }
+        if (_isGrounded) {
+            _currNumOfJumps = 0;
+            _playerVelocity.x = moveDirection.x;
+            _playerVelocity.x *= _playerSpeed;
+            _canWallJump = false;
+            _isWallJumping = false;
+        } else if (_isWallJumping) { _playerVelocity.x = _wallCollisionNormal.x * 4; }
 
-    private void MovePlayer(Vector3 moveDirection) {
-        _controller.Move(moveDirection * Time.deltaTime * _playerSpeed);
+        if ((_isGrounded && _playerVelocity.y < 0) || _wallCollisionNormal.y == -1f) { _playerVelocity.y = 0; }
         
-        if (_startJump && _isGrounded) { //first jump
-            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityValue);
-        } else if (_startJump && !_isGrounded && _currNumOfJumps < 2) { //double jump
-            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -2.5f * _gravityValue);
-        }
+        if (_isWallJumping && !_canWallJump) { _playerVelocity.y = Mathf.Sqrt(_jumpHeight * -1.25f * _gravityValue); //wall jumping
+        } else if (_startJump && _isGrounded) { _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityValue); //first jump
+        } else if (_startJump && !_isGrounded && _currNumOfJumps < 2) { _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -2.5f * _gravityValue); } //double jump
         
         if (_startJump) { _startJump = false; }
 
-        _playerVelocity.y += _gravityValue * Time.deltaTime;
-        _controller.Move(_playerVelocity * Time.deltaTime);
+        _playerVelocity.y += _gravityValue * Time.deltaTime; //apply gravity
+        if (!_movementDisabled) { MovePlayer(_playerVelocity); }
     }
+    
+    private void MovePlayer(Vector3 moveVelocity) { _controller.Move(moveVelocity * Time.deltaTime); }
 
     public void EnableMovement() { _movementDisabled = false;}
     public void DisableMovement() { _movementDisabled = true; }
@@ -83,6 +84,22 @@ public class PlayerController : MonoBehaviour {
 
     private void OnTriggerExit(Collider other) {
         if (other.tag.ToLower().StartsWith("moving")) { transform.parent = null; }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit) {
+        // Debug.DrawRay(hit.point, hit.normal, Color.blue);
+        // if (!_isGrounded) { Debug.Log("hit.normal = " + hit.normal); }
+        
+        if (!_isGrounded && hit.transform.CompareTag("JumpableWall")) {
+            _canWallJump = true;
+            _wallCollisionNormal = hit.normal;
+            StartCoroutine(EndIsWallJumping());
+        }
+    }
+
+    private IEnumerator EndIsWallJumping() {
+        yield return new WaitForSeconds(0.15f);
+        _isWallJumping = false;
     }
 
     private void DoNullChecks() {
